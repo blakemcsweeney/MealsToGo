b4157b61f4ecc6ce3308d703c3e509d35ea13811
Blake <blake.mcsweeney@gmail.com>
2023-07-13 22:49:21 +1000

    Day 2

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1}}>
-        <View style={{ padding: 16, backgroundColor: "green"}}>
-          <Text>Search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue"}}>
-          <Text>List</Text>
-        </View>      
-      </SafeAreaView>
-      <StatusBar style="auto" />
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
