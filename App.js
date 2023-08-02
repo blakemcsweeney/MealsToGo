d0ee6e8c0c09e1a9191caf51527edfa08399e3eb
Blake <blake.mcsweeney@gmail.com>
2023-08-02 21:24:11 +1000

    Day 6

--- App.js
+++ App.js
@@ -13,10 +13,29 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 
+import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
+
+
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings"
+};
 
 const Settings = () => <SafeArea><Text>Settings</Text></SafeArea>
 const Map = () => <SafeArea><Text>Map</Text></SafeArea>
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    headerShown: false,
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -36,9 +55,12 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator screenOptions={{
-            headerShown: false
-          }}>
+          <Tab.Navigator 
+          screenOptions={createScreenOptions}
+          tabBarOptions= {{
+              ActiveTintColor: 'tomato',
+              InactiveTintColor: 'gray',
+            }}>
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
